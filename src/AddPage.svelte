d3d05b952c3add818bce7db8fa60813b079b42fa
Richard King <richard@renewableenergyhub.com.au>
2020-12-20 19:46:20 +1100

    handle quotes in description

--- src/AddPage.svelte
+++ src/AddPage.svelte
@@ -55,7 +55,7 @@
           ',' +
           alt_gain +
           ",'" +
-          description +
+          description.replace(/'/g, "''") +
           "','" +
           weather +
           "'," +
@@ -73,7 +73,7 @@
           ',' +
           alt_gain +
           ",'" +
-          description +
+          description.replace(/'/g, "''") +
           "','" +
           weather +
           "'," +
